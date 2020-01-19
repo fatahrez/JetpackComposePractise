98ef9bc4abd67017e079c8d7db07b9c2d445e06a
fatahrez <fertahrez@gmail.com>
2020-01-19 14:29:45 +0300

    upgrade to android studio 4 and run jetpack compose

---
+++ app/src/main/java/com/example/jetpackcomposepractise/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.jetpackcomposepractise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.ui.core.setContent
+
+class MainActivity : AppCompatActivity() {
+//    Tutorial on https://developer.android.com/jetpack/compose/tutorial
+//    Jetpack Compose simplifies and accelerates UI development
+//
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent{
+
+        }
+    }
+}
