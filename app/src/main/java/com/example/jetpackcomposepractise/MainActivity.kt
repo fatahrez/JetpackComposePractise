8616a9a5b12014bea9feba86f11705f97bb94818
fatahrez <fertahrez@gmail.com>
2020-01-19 14:58:22 +0300

    write composable function Greeting

--- app/src/main/java/com/example/jetpackcomposepractise/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepractise/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.jetpackcomposepractise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.Composable
+import androidx.ui.core.Text
 import androidx.ui.core.setContent
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +12,20 @@ class MainActivity : AppCompatActivity() {
 //
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent{
+//        The setContent block defines the activity's layout instead of defining the layout contents with an XML file
+//        This is called a composable function
+//        The Text() function is an example defined by Compose UI library
 
+//      Composable functions can only be called from within the scope of other composable functions.
+        setContent{
+//            Text("Hello compose")
+            Greeting("Android")
         }
+    }
+
+//    To make a Composable, add the @Composable annotation and then define the function which is passed as a name
+    @Composable
+    fun Greeting (name: String) {
+        Text(text = "Hello $name")
     }
 }
